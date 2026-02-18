4c90d80cc44e73b2856e52215a9d0932ad270bb7
JuanBenz17 <marcosmarion021521@gmail.com>
2026-02-18 19:12:02 +0800

    Create Lab1-8.py

---
+++ Lab1/Lab1-8.py
@@ -0,0 +1,13 @@
+# This program computes the number of seconds in a given number of hours.
+
+hours = 2  # Number of hours to convert
+SECONDS_PER_HOUR = 3600  # Constant: number of seconds in one hour
+
+# Calculate total seconds
+total_seconds = hours * SECONDS_PER_HOUR
+
+# Display results
+print("Hours:", hours)
+print("Seconds in Hours:", total_seconds)
+
+print("Goodbye")
